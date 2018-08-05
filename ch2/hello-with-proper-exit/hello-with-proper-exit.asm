0d6aec81421defcce6172ac719828252505988fe
Clay Carpenter <claycarpenter@gmail.com>
2018-08-04 21:45:09 -0400

    Adds hello with proper exit example.

---
+++ ch2/hello-with-proper-exit/hello-with-proper-exit.asm
@@ -0,0 +1,16 @@
+section .data
+message: data 'hello, world!', 10
+
+section .text
+global _start
+
+_start:
+  mov   rax, 1        ; 'write' syscall number
+  mov   rdi, 1        ; stdout file descriptor
+  mov   rsi, message  ; string address
+  mov   rdx, 14       ; string length in bytes
+  syscall
+
+  mov   rax, 60       ; 'exit' syscall number
+  xor   rdi, rdi      ; set rdi to 0
+  syscall
